1d4a0d4ae8cbda9abf1c6a8b8ee607f1464c9115
1355842282 <1355842282@qq.com>
2020-06-04 20:22:20 +0800

    提交新文件

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+use Aww\FireBird\Test;
+
+
+$toolClass = new Test\Fire();
+
+echo $toolClass->index();
+
+
+
---
+++ src/Test/Fire.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Aww\FireBird\Test;
+
+class Fire
+{
+    public function index()
+    {
+        echo 'I`m fire bird';
+    }
+
+}
